d2af092a2189fa4d9927fd97503c33c8e55ab489
niraj-96 <niraj.may@outlook.com>
2020-09-27 23:26:43 +0530

    saved

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ var corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin,X-Requested-with, Content-Type,Accept");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin,X-Requested-with, Content-Type,Accept");
+//   next();
+// });
 
 app.use(formidable());
 mongoose.Promise = global.Promise;

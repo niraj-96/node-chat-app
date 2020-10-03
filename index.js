1eaf47afbf76f2b57e0e844b5be88321b1082fbb
niraj-96 <niraj.may@outlook.com>
2020-10-03 23:57:04 +0530

    saved

--- index.js
+++ index.js
@@ -11,11 +11,10 @@ const corsOptions = {
   origin: "https://angular-9-chat-app.web.app"
 };
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin,X-Requested-with, Content-Type,Accept");
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(cors(corsOptions));
 
 //app.use(formidable());
